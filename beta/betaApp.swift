ba2e716de346b9f0388006297821cdc23eee53ce
ziyiliu02 <ziyi.liu02@gmail.com>
2023-11-03 11:24:03 +0800

    Initial Commit

---
+++ beta/betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  betaApp.swift
+//  beta
+//
+//  Created by Liu Ziyi on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
