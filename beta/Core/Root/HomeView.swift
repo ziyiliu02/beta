4af0264d59ca4d6f527592debdb61ebdfdad0ae3
ziyiliu02 <ziyi.liu02@gmail.com>
2023-11-04 20:25:47 +0800

    - Color

--- beta/Core/Root/HomeView.swift
+++ beta/Core/Root/HomeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Cherrypick Beta")
+        ZStack {
+            Color.theme.yellow
+                .ignoresSafeArea()
+            
+            Text("Cherrypick Beta")
+                .fontWeight(.bold)
+        }
+        .foregroundStyle(Color.white)
     }
 }
 
---
+++ beta/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  beta
+//
+//  Created by Liu Ziyi on 4/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+   
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let red = Color("RedColor")
+    let yellow = Color("YellowColor")
+    let gray = Color("Gray")
+    let lightgray = Color("LightGray")
+    
+}
