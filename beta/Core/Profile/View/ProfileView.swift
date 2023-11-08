abf53f7b2831d4e182b7a2c83b0feb3b9bab4e78
ziyiliu02 <ziyi.liu02@gmail.com>
2023-11-08 15:19:51 +0800

    - Profile View UI

--- beta/Core/Profile/View/ProfileView.swift
+++ beta/Core/Profile/View/ProfileView.swift
@@ -9,7 +9,115 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile")
+        ScrollView {
+            EpisodeCarouselView()
+                .frame(height: 250)
+            
+            HStack(spacing: 20) {
+                ZStack {
+                    Image("profile-image")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 64, height: 64)
+                        .clipShape(Circle())
+                    
+                    Image(systemName: "plus")
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color.white)
+                        .frame(width: 20, height: 20)
+                        .background(Color.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                        .padding(.top, 65)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Name")
+                        .font(.title2)
+                        .bold()
+                    
+                    Text("@username")
+                        .foregroundStyle(Color.theme.gray)
+                    
+                    Text("2023")
+                        .padding(.top, 5)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 40)
+            .padding(.vertical, 40)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Channels")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Text("1")
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color.white)
+                        .frame(width: 20, height: 20)
+                        .background(Color.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+
+                    Image(systemName: "chevron.right")
+                }
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        ForEach(1 ..< 5, id: \.self) { episode in
+                            VStack {
+                                Rectangle()
+                                    .frame(width: 100, height: 110)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("History")
+                    .font(.title2)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack {
+                    HStack {
+                        Text("Nov 2023")
+                            .bold()
+                        
+                        Image(systemName: "chevron.down")
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("Day")
+                            .bold()
+                        
+                        Image(systemName: "chevron.down")
+                    }
+                }
+                
+                Text("8 Nov 2023, Wed")
+                
+                HStack {
+                    Text("8AM")
+                        .padding(.trailing, 10)
+                    
+                    Rectangle()
+                        .frame(width: 280, height: 200)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.vertical)
+            }
+            .padding(.horizontal)
+        }
+        .ignoresSafeArea()
     }
 }
 
